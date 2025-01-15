5677a0d9a4e48ca0d1b260100a79795993445347
SpiceyWolf <parchmentwolf@gmail.com>
2025-01-15 13:44:50 -0500

    Removed Unity Linkage
    Removed all traces of hard Unity binding before proceeding to Godot extension.

--- Unity/Scripts/Editor/TwoPaneSplitView.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine.UIElements;
-
-namespace SK.Libretro.Unity.Editor
-{
-    [UxmlElement]
-    internal partial class TwoPaneSplitView : UnityEngine.UIElements.TwoPaneSplitView
-    {
-    }
-}
